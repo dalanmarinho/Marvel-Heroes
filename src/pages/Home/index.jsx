eb0fef34742313101633ea2660221027fa53f9d2
dalanmarinho <dalanvieira17@gmail.com>
2024-03-13 19:14:12 -0300

    fix: bug app

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,7 +9,7 @@ import Header from "../../components/Header";
 import Filter from "../../components/Filter";
 
 const HomePage = () => {
-  const [personagens, setPersonagem] = useState([]);
+  const [personagens, setPersonagem] = useState(personagensData);
   const [novosPersonagens, setNovosPersonagens] = useState(personagensData);
   const [filtro, setFiltroState] = useState(false);
   const  localStorageData = JSON.parse(localStorage.getItem("personagens"));
